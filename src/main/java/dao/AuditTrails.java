4e17c840bf2a929291db16f6695b505a5b7af351
Aniruddha Bagal <bagalaniruddha@gmail.com>
2024-04-03 22:27:55 +0530

    File Added 'AuditTrails.java'

--- src/main/java/dao/AuditTrails.java
+++ src/main/java/dao/AuditTrails.java
@@ -8,108 +8,79 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.AuditTrails;
-import util.DatabaseUtility;
-
 public class AuditTrailsDAO {
-    private static final String INSERT_AUDIT_TRAIL = "INSERT INTO audit_trails (action_type, entity_type, entity_id, action_timestamp, user_id, action_detail, fk_complaint_id, fk_investigation_id, fk_action_id, fk_communication_log_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    private static final String UPDATE_AUDIT_TRAIL = "UPDATE audit_trails SET action_type = ?, entity_type = ?, entity_id = ?, action_timestamp = ?, user_id = ?, action_detail = ?, fk_complaint_id = ?, fk_investigation_id = ?, fk_action_id = ?, fk_communication_log_id = ? WHERE id = ?";
-    private static final String DELETE_AUDIT_TRAIL = "DELETE FROM audit_trails WHERE id = ?";
-    private static final String GET_AUDIT_TRAIL_BY_ID = "SELECT * FROM audit_trails WHERE id = ?";
-    private static final String GET_ALL_AUDIT_TRAILS = "SELECT * FROM audit_trails";
 
     public void createAuditTrail(AuditTrails auditTrail) throws SQLException {
-        try (Connection connection = DatabaseUtility.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_AUDIT_TRAIL);
-            preparedStatement.setString(1, auditTrail.getActionType());
-            preparedStatement.setString(2, auditTrail.getEntityType());
-            preparedStatement.setInt(3, auditTrail.getEntityId());
-            preparedStatement.setTimestamp(4, auditTrail.getActionTimestamp());
-            preparedStatement.setInt(5, auditTrail.getUserId());
-            preparedStatement.setString(6, auditTrail.getActionDetail());
-            preparedStatement.setInt(7, auditTrail.getFkComplaintId());
-            preparedStatement.setInt(8, auditTrail.getFkInvestigationId());
-            preparedStatement.setInt(9, auditTrail.getFkActionId());
-            preparedStatement.setInt(10, auditTrail.getFkCommunicationLogId());
+        String sql = "INSERT INTO audit_trails(action_type, entity_type, entity_id, action_timestamp, user_id, action_detail, fk_complaint_id, fk_investigation_id, fk_action_id, fk_communication_log_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DatabaseUtility.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, auditTrail.getActionType());
+            pstmt.setString(2, auditTrail.getEntityType());
+            pstmt.setInt(3, auditTrail.getEntityId());
+            pstmt.setTimestamp(4, auditTrail.getActionTimestamp());
+            pstmt.setInt(5, auditTrail.getUserId());
+            pstmt.setString(6, auditTrail.getActionDetail());
+            pstmt.setInt(7, auditTrail.getFkComplaintId());
+            pstmt.setInt(8, auditTrail.getFkInvestigationId());
+            pstmt.setInt(9, auditTrail.getFkActionId());
+            pstmt.setInt(10, auditTrail.getFkCommunicationLogId());
 
-            preparedStatement.executeUpdate();
+            pstmt.executeUpdate();
         }
     }
 
-    public void updateAuditTrail(AuditTrails auditTrail) throws SQLException {
-        try (Connection connection = DatabaseUtility.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_AUDIT_TRAIL);
-            preparedStatement.setString(1, auditTrail.getActionType());
-            preparedStatement.setString(2, auditTrail.getEntityType());
-            preparedStatement.setInt(3, auditTrail.getEntityId());
-            preparedStatement.setTimestamp(4, auditTrail.getActionTimestamp());
-            preparedStatement.setInt(5, auditTrail.getUserId());
-            preparedStatement.setString(6, auditTrail.getActionDetail());
-            preparedStatement.setInt(7, auditTrail.getFkComplaintId());
-            preparedStatement.setInt(8, auditTrail.getFkInvestigationId());
-            preparedStatement.setInt(9, auditTrail.getFkActionId());
-            preparedStatement.setInt(10, auditTrail.getFkCommunicationLogId());
-            preparedStatement.setInt(11, auditTrail.getId());
-
-            preparedStatement.executeUpdate();
-        }
-    }
-
-    public void deleteAuditTrail(int auditTrailId) throws SQLException {
-        try (Connection connection = DatabaseUtility.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_AUDIT_TRAIL);
-            preparedStatement.setInt(1, auditTrailId);
-
-            preparedStatement.executeUpdate();
-        }
-    }
-
-    public AuditTrails getAuditTrailById(int auditTrailId) throws SQLException {
-        AuditTrails auditTrail = null;
-
-        try (Connection connection = DatabaseUtility.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(GET_AUDIT_TRAIL_BY_ID);
-            preparedStatement.setInt(1, auditTrailId);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                auditTrail = mapAuditTrail(resultSet);
-            }
-        }
-
-        return auditTrail;
-    }
-
-    public List<AuditTrails> getAllAuditTrails() throws SQLException {
+    public List<AuditTrails> readAllAuditTrails() throws SQLException {
         List<AuditTrails> auditTrails = new ArrayList<>();
+        String sql = "SELECT * FROM audit_trails";
+        try (Connection conn = DatabaseUtility.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
 
-        try (Connection connection = DatabaseUtility.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_AUDIT_TRAILS);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                AuditTrails auditTrail = mapAuditTrail(resultSet);
+            while (rs.next()) {
+                AuditTrails auditTrail = new AuditTrails(
+                        rs.getInt("id"),
+                        rs.getString("action_type"),
+                        rs.getString("entity_type"),
+                        rs.getInt("entity_id"),
+                        rs.getTimestamp("action_timestamp"),
+                        rs.getInt("user_id"),
+                        rs.getString("action_detail"),
+                        rs.getInt("fk_complaint_id"),
+                        rs.getInt("fk_investigation_id"),
+                        rs.getInt("fk_action_id"),
+                        rs.getInt("fk_communication_log_id")
+                );
                 auditTrails.add(auditTrail);
             }
         }
-
         return auditTrails;
     }
 
-    private AuditTrails mapAuditTrail(ResultSet resultSet) throws SQLException {
-        int id = resultSet.getInt("id");
-        String actionType = resultSet.getString("action_type");
-        String entityType = resultSet.getString("entity_type");
-        int entityId = resultSet.getInt("entity_id");
-        Timestamp actionTimestamp = resultSet.getTimestamp("action_timestamp");
-        int userId = resultSet.getInt("user_id");
-        String actionDetail = resultSet.getString("action_detail");
-        int fkComplaintId = resultSet.getInt("fk_complaint_id");
-        int fkInvestigationId = resultSet.getInt("fk_investigation_id");
-        int fkActionId = resultSet.getInt("fk_action_id");
-        int fkCommunicationLogId = resultSet.getInt("fk_communication_log_id");
+    public void updateAuditTrail(AuditTrails auditTrail) throws SQLException {
+        String sql = "UPDATE audit_trails SET action_type = ?, entity_type = ?, entity_id = ?, action_timestamp = ?, user_id = ?, action_detail = ?, fk_complaint_id = ?, fk_investigation_id = ?, fk_action_id = ?, fk_communication_log_id = ?";
+        try (Connection conn = DatabaseUtility.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, auditTrail.getActionType());
+            pstmt.setString(2, auditTrail.getEntityType());
+            pstmt.setInt(3, auditTrail.getEntityId());
+            pstmt.setTimestamp(4, auditTrail.getActionTimestamp());
+            pstmt.setInt(5, auditTrail.getUserId());
+            pstmt.setString(6, auditTrail.getActionDetail());
+            pstmt.setInt(7, auditTrail.getFkComplaintId());
+            pstmt.setInt(8, auditTrail.getFkInvestigationId());
+            pstmt.setInt(9, auditTrail.getFkActionId());
+            pstmt.setInt(10, auditTrail.getFkCommunicationLogId());
 
-        return new AuditTrails(id, actionType, entityType, entityId, actionTimestamp, userId, actionDetail, fkComplaintId, fkInvestigationId, fkActionId, fkCommunicationLogId);
+            pstmt.executeUpdate();
+        }
+    }
+
+    public void deleteAuditTrail(int id) throws SQLException {
+        String sql = "DELETE FROM audit_trails WHERE id = ?";
+        try (Connection conn = DatabaseUtility.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        }
     }
 }
